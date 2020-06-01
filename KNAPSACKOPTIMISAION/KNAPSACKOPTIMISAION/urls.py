65e82bd53681dd0b9617d21d9d7f5e7cb2b97ecc
DallelKh <gd_khezzane@esi.dz>
2020-06-01 20:19:22 +0100

    adding class

--- KNAPSACKOPTIMISAION/KNAPSACKOPTIMISAION/urls.py
+++ KNAPSACKOPTIMISAION/KNAPSACKOPTIMISAION/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('algoritmmanagement.urls')),
+    path('knapsack/', include('algoritmmanagement.urls')),
     
     
 ]
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # KnapsackOptimisation
 simple web app for execution of diffrent combinatorial optimization algorithms on The knapsack problem
+1)Branch & Bound
+2)Heuristics
+3)Meta-Heuristics
